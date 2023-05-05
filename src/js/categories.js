d347fef48e876fa2612f988eaaa90de2921b932b
barkode <konstantinks@gmail.com>
2023-05-05 18:50:46 +0100

    add category list

---
+++ src/js/categories.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+const category_list = document.querySelector('.nav-categories');
+
+async function getCategoryList() {
+  const { data } = await axios.get(
+    'https://books-backend.p.goit.global/books/category-list'
+  );
+  return data;
+}
+
+const renderCategories = async () => {
+  try {
+    const category = await getCategoryList();
+    category_list.innerHTML = await markupCategoriesList(category);
+    const listCategory = document.querySelectorAll('.category-item');
+    listCategory.forEach(itemCategory => {
+      itemCategory.addEventListener('click', event => {
+        const ActiveCategory = document.querySelector('.category-item.active');
+        if (ActiveCategory) {
+          ActiveCategory.classList.remove('active');
+        }
+        event.target.classList.add('active');
+      });
+    });
+  } catch (error) {
+    console.log('Oops! Something went wrong');
+  }
+};
+
+renderCategories();
+
+function markupCategoriesList(categories) {
+  return `<li class="category-item active" data-id="all-categories">
+        All categories</li>
+        ${categories
+          .map(
+            category => `<li class="category-item" data-id="${category.list_name}">
+        ${category.list_name}
+        </li>`
+          )
+          .join('')}`;
+}
