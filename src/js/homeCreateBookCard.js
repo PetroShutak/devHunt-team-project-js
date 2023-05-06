cedaeffa83766e5a2959b953f04ea69b23ac11c2
AndriiZlt <andreyzlot@gmail.com>
2023-05-06 11:00:36 +0300

    Quick view on book card

--- src/js/homeCreateBookCard.js
+++ src/js/homeCreateBookCard.js
@@ -1,7 +1,7 @@
 export function createBookCard(book) {
   return `  <a class="gallery-book-link" href="">
               <div class="gallery-book-card">
-                <img class="gallery-book-cover" src="${book.book_image}"></img>
+              <div class="gallery-book-cover" style="background-image: url('${book.book_image}')"></div>
                 <p class="gallery-book-name">${book.title}</p>
                 <p class="gallery-author">${book.author}</p>
               </div>
@@ -11,7 +11,7 @@ export function createBookCard(book) {
 export function createHomeBookCard(book) {
   return `  <a class="gallery-book-home-link" href="">
               <div class="gallery-book-card">
-                <img class="gallery-book-cover" src="${book.book_image}"></img>
+                <div class="gallery-book-cover" style="background-image: url('${book.book_image}')"></div>
                 <p class="gallery-book-name">${book.title}</p>
                 <p class="gallery-author">${book.author}</p>
               </div>
