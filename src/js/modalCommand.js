1070483443f67a1e713ffc22543a5859bd1ea5bf
petekostrytsya <petekostrytsya@gmail.com>
2023-05-10 18:09:46 +0300

    addPhoto$4#3

--- src/js/modalCommand.js
+++ src/js/modalCommand.js
@@ -4,7 +4,7 @@ const markupTeamInfo = teamMembers.reduce((acc, item) => {
   const { memberName, photo, github, linkedIn, width, height } = item;
 
   return (acc += ` <li class="employees-card">
-  <img
+  <img class="employees-img"
     src="${photo}"
     alt="${memberName}"
     width="${width}"
@@ -14,14 +14,14 @@ const markupTeamInfo = teamMembers.reduce((acc, item) => {
   <ul class="social-list">
     <li class="social-list-itam">
       <a href="${github}" class="social-list-link">
-        <svg class="social-list-icon" width="16" height="16">
+        <svg class="social-list-icon" width="24" height="24">
           <use href="../images/sprite.svg#icon-github"></use>
         </svg>
       </a>
     </li>
     <li class="social-list-itam">
       <a href="${linkedIn}" class="social-list-link">
-        <svg class="social-list-icon" width="16" height="16">
+        <svg class="social-list-icon" width="24" height="24">
           <use href="../images/sprite.svg#icon-linkedin"></use>
         </svg>
       </a>
