cef80de8a397a300a8aab6b30ab3f82e79f99f6b
Serhii <116863810+Serhii-K5@users.noreply.github.com>
2023-05-10 18:54:36 +0300

    fixed modal menu bug
    *  Виправлено підсвічування активної сторнки в моб. меню;
    *  виправлено помилки  лого при зміні розмірів в'юпорта;
    *  підчистив свій код

--- src/js/light-dark-theme.js
+++ src/js/light-dark-theme.js
@@ -1,9 +1,6 @@
-// import logos from '../images/sprite.svg#icon-logo';
-// import logos1 from '../images/sprite.svg#icon-logo-1';
 const inputTheme = document.querySelector('.div-theme');
 const spanTheme = document.querySelector('.span-theme');
 
-// v1
 const body = document.querySelector('body');
 let indexTheme = false;
 
@@ -29,20 +26,14 @@ function currentTheme() {
 
   const logo = document.querySelector('.header-logo-icon');
   const logo1 = document.querySelector('.header-logo-icon1');
-  // const burger = document.querySelector('.header-burger-icon');
-  // const burger1 = document.querySelector('.header-burger-icon1');
   if (indexTheme) {
     body.classList.add('dark-theme');
     spanTheme.style.left = '20px';
-    // logo.style.backgroundImage = `url(${logos})`;
-    // logo.innerHTML = `<svg><use href="${logos1}"></use></svg>`;
     logo.style.width = '0px';
     logo1.style.width = '109px';
   } else {
     body.classList.remove('dark-theme');
     spanTheme.style.left = '2px';
-    // logo.style.backgroundImage = `url(${logos1})`
-    // logo.innerHTML = `<svg><use href="${logos}"></use></svg>`;
     logo1.style.width = '0px';
     logo.style.width = '109px';
   }
@@ -52,12 +43,15 @@ currentTheme();
 
 // виділення сторінок при перемиканні
 const headerNavLinks = document.querySelector('.header-nav-item').querySelectorAll('a');
+const headerNavLinksModal = document.querySelector('.header-nav-item-modal').querySelectorAll('a');
 if (document.querySelector('.home-page') === null) {
   for (let index = 0; index < headerNavLinks.length; index++) {
-    headerNavLinks[index].classList.toggle("heder-active");    
+    headerNavLinks[index].classList.toggle("heder-active"); 
+    headerNavLinksModal[index].classList.toggle("heder-active");
   }
 }
 
+// console.log(ActiveXObject);
 // const nameShopBookLinks = document.querySelector('.shop-book').querySelectorAll('.name-shop-book');
 // if (nameShopBookLinks.length > 0) {
 //   for (let index = 0; index < nameShopBookLinks.length; index++) {
