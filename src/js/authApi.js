824aa302a88a6f5b96a8989ee687514d661384c6
barkode <konstantinks@gmail.com>
2023-05-10 19:55:09 +0100

    Update authApi.js

--- src/js/authApi.js
+++ src/js/authApi.js
@@ -3,7 +3,7 @@ import { getAuthForm } from '../js/authForm';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-
+import { getDatabase } from 'firebase/database';
 getAuthForm();
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -22,6 +22,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getDatabase(app);
 
 console.log(app);
 console.log(auth);
+console.log(db);
