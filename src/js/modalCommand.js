d558afc60a1f695cd61b240231a63f3108677872
barkode <konstantinks@gmail.com>
2023-05-10 10:13:34 +0100

    html to js
    Переписано на js. Прибрано html.

--- src/js/modalCommand.js
+++ src/js/modalCommand.js
@@ -1,12 +1,36 @@
-import photo1 from '../img_command_photo/petro-shutak.jpg';
-import photo2 from '../img_command_photo/tatiana-tereshchenko.jpg';
-import photo3 from '../img_command_photo/petro-shutak.jpg';
-import photo4 from '../img_command_photo/andrii-zlotnikov.jpg';
-import photo5 from '../img_command_photo/yuliia-karas.jpg';
-import photo6 from '../img_command_photo/kostiantyn-krysenko.jpg';
-import photo7 from '../img_command_photo/anatolii-shkurpela.jpg';
-import photo8 from '../img_command_photo/yuliia-usova.jpg';
-import photo9 from '../img_command_photo/petro-kostrytsia.jpg';
+import { teamMembers } from './team_info';
+import no_photo from '../img_command_photo/no_photo.jpg';
+console.log(teamMembers);
 
+const markupTeamInfo = teamMembers.reduce((acc, item) => {
+  const { memberName, photo, github, linkedIn } = item;
 
+  return (acc += ` <li class="employees-card">
+  <img
+    src="${photo}"
+    alt="${memberName}"
+    width="40"
+    height="60"
+  />
+  <h3 class="employees-name">${memberName}</h3>
+  <ul class="social-list">
+    <li class="social-list-itam">
+      <a href="${github}" class="social-list-link">
+        <svg class="social-list-icon" width="16" height="16">
+          <use href="../images/sprite.svg#icon-github"></use>
+        </svg>
+      </a>
+    </li>
+    <li class="social-list-itam">
+      <a href="${linkedIn}" class="social-list-link">
+        <svg class="social-list-icon" width="16" height="16">
+          <use href="../images/sprite.svg#icon-linkedin"></use>
+        </svg>
+      </a>
+    </li>
+  </ul>
+</li>`);
+}, '');
 
+const team = document.querySelector('.employees-item');
+team.insertAdjacentHTML('beforebegin', markupTeamInfo);
---
+++ src/js/team_info.js
@@ -0,0 +1,66 @@
+import photo1 from '../img_command_photo/petro-shutak.jpg';
+import photo2 from '../img_command_photo/tatiana-tereshchenko.jpg';
+import photo3 from '../img_command_photo/andrii-zlotnikov.jpg';
+import photo4 from '../img_command_photo/yuliia-karas.jpg';
+import photo5 from '../img_command_photo/kostiantyn-krysenko.jpg';
+import photo6 from '../img_command_photo/anatolii-shkurpela.jpg';
+import photo7 from '../img_command_photo/yuliia-usova.jpg';
+import photo8 from '../img_command_photo/petro-kostrytsia.jpg';
+import no_photo from '../img_command_photo/no_photo.jpg';
+
+export const teamMembers = [
+  {
+    memberName: 'Petro Shutak',
+    photo: photo1,
+    github: '',
+    linkedIn: '',
+  },
+  {
+    memberName: 'Tatiana-Tereshchenko',
+    photo: photo2,
+    github: '',
+    linkedIn: '',
+  },
+  {
+    memberName: 'Sergii Kotkov',
+    photo: no_photo,
+    github: '',
+    linkedIn: '',
+  },
+  {
+    memberName: 'Andrii Zlotnikov',
+    photo: photo3,
+    github: '',
+    linkedIn: '',
+  },
+  {
+    memberName: 'Yuliia Karas',
+    photo: photo4,
+    github: '',
+    linkedIn: '',
+  },
+  {
+    memberName: 'Kostiantyn Krysenko',
+    photo: photo5,
+    github: '',
+    linkedIn: '',
+  },
+  {
+    memberName: 'Anatolii Shkurpela',
+    photo: photo6,
+    github: '',
+    linkedIn: '',
+  },
+  {
+    memberName: 'Yuliia Usova',
+    photo: photo7,
+    github: '',
+    linkedIn: '',
+  },
+  {
+    memberName: 'Petro Kostrytsia',
+    photo: photo8,
+    github: '',
+    linkedIn: '',
+  },
+];
