e9f12e7264e07d9b29aac82b1211cfcee3f173f3
PetroShutak <113899587+PetroShutak@users.noreply.github.com>
2023-05-11 10:22:56 +0000

    Deploying to gh-pages from @ PetroShutak/devHunt-team-project-js@2d4c6d6753455d02bcc6fa4e1fe6fdafd66f11b8 🚀

--- assets/light-dark-theme-572dd23e.js
+++ assets/light-dark-theme-572dd23e.js
@@ -24,7 +24,7 @@ Notiflix needs to be appended to the "<`+s+'>" element, but you called it before
       <p class="shopping-list-book-author">${i.author}</p>
       <ul class="shopping-list-trading">
         <li class="shopping-list-trading-item">
-          <a class="shopping-list-trading-link">
+          <a class="shopping-list-trading-link" href="${i.buy_links[0].url}" target="_blank">
             <img
               src="${Ui}"
               class="shopping-list-trading-icon-amazon"
@@ -32,7 +32,7 @@ Notiflix needs to be appended to the "<`+s+'>" element, but you called it before
           </a>
         </li>
         <li class="shopping-list-trading-item">
-          <a class="shopping-list-trading-link">
+          <a class="shopping-list-trading-link" href="${i.buy_links[1].url}" target="_blank">
             <img
               src="${ji}"
               class="shopping-list-trading-icon-apple-books"
@@ -40,7 +40,7 @@ Notiflix needs to be appended to the "<`+s+'>" element, but you called it before
           </a>
         </li>
         <li class="shopping-list-trading-item">
-          <a class="shopping-list-trading-link">
+          <a class="shopping-list-trading-link" href="${i.buy_links[2].url}" target="_blank">
             <img
               src="${Pi}"
               class="shopping-list-trading-icon-book-shop"
