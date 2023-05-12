edbe50b3571dc8c89df976cbe71a2b0d89c59f64
AndriiZlt <andreyzlot@gmail.com>
2023-05-12 12:33:38 +0300

    Update renderingByCategory.js

--- src/js/renderingByCategory.js
+++ src/js/renderingByCategory.js
@@ -9,6 +9,8 @@ export default function renderingByCategory(e) {
   console.log('Rendering by category');
 
   galleryRef.innerHTML = '';
+
+  // Click on See-more button
   if (e.target.innerHTML === 'See more') {
     galleryRef.insertAdjacentHTML(
       'beforeend',
@@ -23,6 +25,7 @@ export default function renderingByCategory(e) {
       'beforeend',
       `<div class="gallery-list2"></div>`
     );
+
     var galleryListRef = document.querySelector('.gallery-list2');
 
     const query = e.target.dataset.category.split(' ').join('%20');
@@ -38,18 +41,20 @@ export default function renderingByCategory(e) {
     document
       .querySelector(`[data-id="${e.target.dataset.category}"]`)
       .classList.add('active');
-
     // scrolling to top
     window.scrollTo(0, 0);
     return;
   }
+
+  // Click on "All categories" in categories section
   if (e.target.innerHTML.trim() === 'All categories') {
     renderingHomePage();
-
     // scrolling to top
     window.scrollTo(0, 0);
     return;
   }
+
+  // Header of the book section
   galleryRef.insertAdjacentHTML(
     'beforeend',
     `<h2 class="gallery-title">${e.target.innerHTML
@@ -65,7 +70,6 @@ export default function renderingByCategory(e) {
     'beforeend',
     `<div class="gallery-list2"></div>`
   );
-  var galleryListRef = document.querySelector('.gallery-list2');
 
   const query = e.target.innerHTML.trim().split(' ').join('%20');
   fetchingByCategory(query).then(response => {
